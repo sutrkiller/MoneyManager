95107cb767656a8aeb38d43efed046b74f6484d8
sutrkiller <tobias.kamenicky@gmail.com>
2016-06-07 15:26:18 +0200

    exchange class added...now

---
+++ app/src/main/java/pv239/fi/muni/cz/moneymanager/helper/ExchangeRateCalculator.java
@@ -0,0 +1,77 @@
+package pv239.fi.muni.cz.moneymanager.helper;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponse;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.Currency;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Created by Tobias on 6/7/2016.
+ */
+public class ExchangeRateCalculator {
+
+
+
+    private static class HttpAsyncTask extends AsyncTask<Currency, Void, BigDecimal> {
+        @Override
+        protected BigDecimal doInBackground(Currency... urls) {
+
+            String url  = "http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=";
+            String charset = "UTF-8";
+            String query="";
+            try {
+                query  = String.format("%s%s=X",
+                        URLEncoder.encode(urls[0].getCurrencyCode(), charset),
+                        URLEncoder.encode(urls[1].getCurrencyCode(), charset));
+            } catch (UnsupportedEncodingException e) {
+                Log.e("Error parsing",Log.getStackTraceString(e));
+                return BigDecimal.ONE;
+            }
+            try {
+                URLConnection connection = new URL(url  + query).openConnection();
+                connection.setRequestProperty("Accept-Charset", charset);
+                InputStream response = connection.getInputStream();
+                Scanner scannerr = new Scanner(response);
+                String responseBody = scannerr.useDelimiter(",").next();
+                return scannerr.nextBigDecimal();
+            } catch (IOException e) {
+                Log.e("Error connection",Log.getStackTraceString(e));
+                return BigDecimal.ONE;
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(BigDecimal result) {
+
+        }
+    }
+
+    public static BigDecimal TransferRate(@Nonnull Currency curFrom,@Nonnull Currency curTo, BigDecimal value) {
+        try {
+            return new HttpAsyncTask().execute(curFrom,curTo).get().multiply(value);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
